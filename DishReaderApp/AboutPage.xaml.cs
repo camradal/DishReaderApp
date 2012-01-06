4b4013b81b70b38520c57abbb75a39c95e55330d
Alexey Kalinin <camradal@yahoo.com>
2012-01-06 12:25:30 -0800

    finished about page

--- DishReaderApp/AboutPage.xaml.cs
+++ DishReaderApp/AboutPage.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Windows.Controls.Primitives;
+using Microsoft.Phone.Tasks;
+using System.IO;
 
 namespace DishReaderApp
 {
@@ -18,6 +21,75 @@ namespace DishReaderApp
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            ReadVersionFromManifest();
+        }
+
+        private void feedbackButton_Click(object sender, RoutedEventArgs e)
+        {
+            EmailComposeTask task = new EmailComposeTask();
+            task.Subject = "Feedback about Dish Reader";
+            task.Body = "Hello, I've been using Dish Reader and I have following feedback\n\n";
+            task.To = "dapperpanda@gmail.com";
+            task.Show();
+        }
+
+        private void rateButton_Click(object sender, RoutedEventArgs e)
+        {
+            MarketplaceReviewTask task = new MarketplaceReviewTask();
+            task.Show();
+        }
+
+        private void ReadVersionFromManifest()
+        {
+            Uri manifest = new Uri("WMAppManifest.xml", UriKind.Relative);
+            var si = Application.GetResourceStream(manifest);
+            if (si != null)
+            {
+                using (StreamReader sr = new StreamReader(si.Stream))
+                {
+                    bool haveApp = false;
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        if (!haveApp)
+                        {
+                            int i = line.IndexOf("AppPlatformVersion=\"", StringComparison.InvariantCulture);
+                            if (i >= 0)
+                            {
+                                haveApp = true;
+                                line = line.Substring(i + 20);
+
+                                int z = line.IndexOf("\"");
+                                if (z >= 0)
+                                {
+                                    // if you're interested in the app plat version at all
+                                    // AppPlatformVersion = line.Substring(0, z);
+                                }
+                            }
+                        }
+
+                        int y = line.IndexOf("Version=\"", StringComparison.InvariantCulture);
+                        if (y >= 0)
+                        {
+                            int z = line.IndexOf("\"", y + 9, StringComparison.InvariantCulture);
+                            if (z >= 0)
+                            {
+                                // We have the version, no need to read on.
+                                versionText.Text = line.Substring(y + 9, z - y - 9);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                versionText.Text = "Unknown";
+            }
         }
     }
 }
